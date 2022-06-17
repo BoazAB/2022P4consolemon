5fd53d2b954849214d920bc4bc7b5e68ed443911
Blauwberry <32714@ma-web.nl>
2022-06-17 11:38:33 +0200

    vaker pushen

---
+++ ConsoleMon/ConsoleMon/ConsoleMon.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    class ConsoleMon
+    {
+        private int hp;
+        private float energy;
+        private string name;
+        private string weakness = "Elementen.water";
+
+        List<Skill> Skills = new List<Skill>();
+        class Skills
+        {
+
+        }
+
+        public void TakeDamage(int damage)
+        {
+            hp = (hp - damage);
+        }
+        public void DepleteEnergy(int energy)
+        {
+            this.energy = this.energy - energy;
+        }
+    }
+
+    internal enum Elementen
+    {
+        fire,
+        water,
+        earth,
+        air,
+        wood,
+        metal
+    }
+}
---
+++ ConsoleMon/ConsoleMon/Skill.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    class Skill
+    {
+        private int damage;
+        private int energyCost;
+        private string name;
+        private string element = "Elementen.fire";
+
+        public void UseOn(ConsoleMon target, ConsoleMon caster)
+        {
+            caster.DepleteEnergy(energyCost);
+            target.TakeDamage(damage);
+        }
+
+        internal Skill()
+        {
+
+        }
+
+        internal Skill()
+        {
+
+        }
+    }
+}
